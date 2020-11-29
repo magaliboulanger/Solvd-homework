608fb1a005476c66ffd469ce9e3e5e7e10bac509
magaliboulanger <maga.boulanger8@gmail.com>
2020-11-29 18:39:29 -0300

    fixed problems

---
+++ src/main/java/com/solvd/university/model/Person.java
@@ -0,0 +1,33 @@
+package com.solvd.university.model;
+
+public class Person extends AbstractEntity{
+	private String name;
+	private String phone;
+	private String address;
+	public Person(long id, String name, String phone, String address) {
+		super(id);
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
--- src/main/java/com/solvd/university/model/Student.java
+++ src/main/java/com/solvd/university/model/Student.java
@@ -1,36 +1,14 @@
 package com.solvd.university.model;
 
-public class Student extends AbstractEntity {
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", phone=" + phone + ", address=" + address + "]";
+public class Student extends Person {
+
+	private int universityIdentifier;
+	
+	public Student(long id, String name, String phone, String address, int ui) {
+		super(id, name, phone, address);
+		this.universityIdentifier=ui;
 	}
-	private String name;
-	private String phone;
-	private String address;
-	public Student(long id, String name, String phone, String address) {
-		super(id);
-		this.name = name;
-		this.phone = phone;
-		this.address = address;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+
+	
 	
 }
